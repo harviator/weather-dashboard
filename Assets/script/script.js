29f081241c665536821b71b22db4c91c964c43dc
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-30 19:01:53 -0400

    following the steps for the open weather API key - created a variable to store the API urland started a fetch

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,3 +1,17 @@
 //OpenWeather API KEY: aef0b3e494e18420468bba6b1a3ed89b
 
 var openWxAPIKey = "aef0b3e494e18420468bba6b1a3ed89b";
+
+//Variable to collect user input for the city name and store it
+var city;
+
+//Variable for search query
+var queryURL = `api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${openWxAPIKey}`; //http? can concatenate city and API Key if it doesn't work this way
+
+//Fetch
+fetch(queryURL)
+
+/*
+Notes:
+1. Still need to create input to get user name for fetch to work.
+*/
