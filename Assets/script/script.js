75f323c0054a7aff6b70a549c868f880befaa208
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-30 20:23:38 -0400

    Tested fetch and I get the data in the console. Now that I have some basics going I'm going to review the readme for the homework and pseudocode to get a better framework in my mind

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -5,11 +5,28 @@ var openWxAPIKey = "aef0b3e494e18420468bba6b1a3ed89b";
 //Variable to collect user input for the city name and store it
 var city;
 
-//Variable for search query
-var queryURL = `api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${openWxAPIKey}`; //http? can concatenate city and API Key if it doesn't work this way
+city = 'Toronto';
 
-//Fetch
-fetch(queryURL)
+
+
+//Fetch function
+function getWx() {
+
+    //Variable for search query
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${openWxAPIKey}`;
+    console.log(queryURL);
+
+    fetch(queryURL)
+        .then(function (data) {
+            return data.json();
+        })
+        .then (function (parsedData) {
+            console.log(parsedData);
+        });
+}
+
+getWx();
+
 
 /*
 Notes:
